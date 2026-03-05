e8d287e246d7800ebdb7ccfc897948d3df8a2c62
Alf33140 <alf2kdavy@yahoo.com>
2026-03-05 09:47:32 +0100

    First commit bibliotheque

---
+++ src/Entity/Book.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $auteur = null;
+
+    #[ORM\Column(length: 13)]
+    private ?string $isbn = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $stock = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): static
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): static
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): static
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(?int $stock): static
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Emprunt.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmpruntRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EmpruntRepository::class)]
+class Emprunt
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $dateEmprunt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $dateRetour = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $statut = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateEmprunt(): ?\DateTimeImmutable
+    {
+        return $this->dateEmprunt;
+    }
+
+    public function setDateEmprunt(\DateTimeImmutable $dateEmprunt): static
+    {
+        $this->dateEmprunt = $dateEmprunt;
+
+        return $this;
+    }
+
+    public function getDateRetour(): ?\DateTimeImmutable
+    {
+        return $this->dateRetour;
+    }
+
+    public function setDateRetour(?\DateTimeImmutable $dateRetour): static
+    {
+        $this->dateRetour = $dateRetour;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+}
